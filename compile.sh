e973d974db5f98d038fd73e681f45e80cac76858
Agustín <agustinf@avature.net>
2007-12-31 08:19:00 -0300

    Removed duplicate file from compile.sh

--- compile.sh
+++ compile.sh
@@ -7,7 +7,6 @@ cat var.js >> log.js
 cat extra.js >> log.js
 cat guess.js >> log.js
 cat diff.js >> log.js
-cat logItem.js >> log.js
 cat Console.js >> log.js
 cat logItem.js >> log.js
 cat LogPanel.js >> log.js
